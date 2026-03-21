efc3418eacfbd74c9980d638093ec8ce31e4797f
Andre Correia <andrecorreialagos@gmail.com>
2026-03-21 19:07:25 +0000

    Level Flows

--- scenes/game_scene/levels/AIScanAnim.gd
+++ scenes/game_scene/levels/AIScanAnim.gd
@@ -1,10 +1,12 @@
 extends VBoxContainer
 
 @onready var warningLabel = $Warning
-@onready var codeBlockLabel = $"Code Block"
+@onready var codeBlockLabel = $"HBoxContainer/Code Block"
+@onready var winloseLog = $HBoxContainer/Log
 
 var maxLines = 5
 var terminalLines = []
+var logLines = []
 var warningMessage = "WARNING: AI scan initialised..."
 var fakeLogData = [
 	"Loading core drivers...",
@@ -18,12 +20,27 @@ var fakeLogData = [
     "Scan complete."
 ]
 
+var fakeWinLogData = [
+	"No issues found",
+	"Certainty: 98%",
+	"Next exhibit",
+	"Press any key to continue"
+]
+
+var fakeLoseLogData = [
+	"ISSUES DETECTED",
+	"Match: 98%",
+	"Eliminate TARGET",
+	"Press any key to be murdered"
+]
+
 func _ready():
 	beginScan()
 
 func beginScan():
 	warningLabel.text = ""
 	codeBlockLabel.text = ""
+	winloseLog.text = ""
 	await writeWarning()
 	await writeTerminalOutput()
 
@@ -46,3 +63,45 @@ func writeTerminalOutput():
 			
 		codeBlockLabel.text = currentBlockText
 		await get_tree().create_timer(0.4).timeout
+
+func writeWin():
+	for line in fakeWinLogData:
+		var currentBlockText = ""
+		for i in range(logLines.size()):
+			currentBlockText += logLines[i] + "\n"
+		
+		var stringBuilder = ""
+		for character in line:
+			stringBuilder += character
+			winloseLog.text = currentBlockText + stringBuilder
+			await get_tree().create_timer(0.05).timeout
+		
+		logLines.append(line)
+		if logLines.size() > maxLines:
+			logLines.pop_front()
+			
+	while Input.is_anything_pressed():
+		await get_tree().process_frame
+	while not Input.is_anything_pressed():
+		await get_tree().process_frame
+
+func writeLoss():
+	for line in fakeLoseLogData:
+		var currentBlockText = ""
+		for i in range(logLines.size()):
+			currentBlockText += logLines[i] + "\n"
+		
+		var stringBuilder = ""
+		for character in line:
+			stringBuilder += character
+			winloseLog.text = currentBlockText + stringBuilder
+			await get_tree().create_timer(0.05).timeout
+		
+		logLines.append(line)
+		if logLines.size() > maxLines:
+			logLines.pop_front()
+			
+	while Input.is_anything_pressed():
+		await get_tree().process_frame
+	while not Input.is_anything_pressed():
+		await get_tree().process_frame
--- scenes/game_scene/levels/level.gd
+++ scenes/game_scene/levels/level.gd
@@ -4,17 +4,28 @@ signal level_lost
 @warning_ignore("unused_signal")
 @onready var detectionSys = $Detection
 @onready var countDownTimer = $"Game UI/VBoxContainer/CountDown/Timer"
-@export var time = 15
+@export var time = 18
 @export_file("*.tscn") var next_level_path : String
 @onready var scanUI = $"AI Scan UI"
 @onready var gameUI = $"Game UI"
 @onready var ai = $"AI Scan UI/VBoxContainer"
 var level_state : LevelState
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
 
 func resetLevel() -> void:
 	get_tree().reload_current_scene()
+	
+func setBrightness(targetValue : float) -> void:
+	$CanvasLayer2/ColorRect.material.set_shader_parameter("brightness", targetValue)
+
+func fadeBrightness(targetValue : float, duration : float) -> void:
+	var tween = get_tree().create_tween()
+	var startValue = $CanvasLayer2/ColorRect.material.get_shader_parameter("brightness")
+	tween.tween_method(setBrightness, startValue, targetValue, duration)
+	await tween.finished
 
 func _ready() -> void:
+	animation_player.play_backwards("fade")
 	level_state = GameState.get_level_state(scene_file_path)
 	countDownTimer.start(time)
 	await countDownTimer.timeout
@@ -24,6 +35,15 @@ func _ready() -> void:
 	await $"AI Scan UI/VBoxContainer".beginScan()
 	#detectionSys.showBoxes()
 	if detectionSys.checkWinCondition():
+		await $"AI Scan UI/VBoxContainer".writeWin()
+		animation_player.play("fade")
+		await animation_player.animation_finished
 		get_tree().change_scene_to_file(next_level_path)
 	else:
+		await $"AI Scan UI/VBoxContainer".writeLoss()
+		animation_player.play("fade")
+		await animation_player.animation_finished
 		resetLevel()
+
+#func _exit_tree() -> void:
+#	animation_player.play("fade")
