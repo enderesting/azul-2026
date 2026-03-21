2479fbf7fb0c3394d4b74c9d1c9b04d886408566
Darth712 <nicolaslv10@gmail.com>
2026-03-21 14:50:15 +0000

    fix some detection logic

--- scenes/game_scene/levels/detection.gd
+++ scenes/game_scene/levels/detection.gd
@@ -1,18 +1,41 @@
 extends Node2D
 
-
-@export var body_part: Node2D 
-@export var target_point: Node2D 
 @export var trigger_distance: float = 200.0
-# Called when the node enters the scene tree for the first time.
+
+
+var body_parts: Array[Node] = []
+
+var target_nodes: Array[Node] = []
+var distances: Array[int] = [100,100,100,100,100]
+
 func _ready() -> void:
-	pass # Replace with function body.
+	body_parts = get_tree().get_nodes_in_group("player_points")
+	
+	target_nodes = get_children()
 
+func _process(_delta: float) -> void:
+	for target in target_nodes:
+		if not target is Node2D:
+			continue
+		for part in body_parts:
+			if part.name == target.name:
+				var distance = target.global_position.distance_to(part.global_position)
+			
+				
+				if distance < trigger_distance:
+					_handle_proximity(part, target, distance)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if body_part and target_point:
-		var distance = global_position.distance_to(body_part.global_position)
-		
-		if distance < trigger_distance:
-			print("Player is close! Distance: ", distance)
+func _handle_proximity(part: Node2D, target: Node2D, dist: float):
+	match target.name:
+		"Head":
+			pass
+			#print("Proximity: ", target.name, " is ", dist, " units away.") 
+		"LHand":
+			pass
+			#print("Proximity: ", target.name, " is ", dist, " units away.")
+		"RHand":
+			print("Proximity: ", target.name, " is ", dist, " units away.")
+		"LFoot":
+			print("Proximity: ", target.name, " is ", dist, " units away.")
+		"RFoot":
+			print("Proximity: ", target.name, " is ", dist, " units away.")
