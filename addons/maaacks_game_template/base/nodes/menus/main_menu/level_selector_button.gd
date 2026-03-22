d4331b2783f9a49dc4ca0b5f52db0a6cabc96f3c
IsaacCerejo <isaacerejo2@gmail.com>
2026-03-22 13:09:01 +0000

    add: level selector

--- addons/maaacks_game_template/base/nodes/menus/main_menu/level_selector_button.gd
+++ addons/maaacks_game_template/base/nodes/menus/main_menu/level_selector_button.gd
@@ -1,9 +1,14 @@
 extends Button
 
+@onready var screenshot_node: TextureRect = %screenshot
 
 @export var level_packed = PackedScene
 
+@export var screenshot: Texture2D
 
+func _ready() -> void:
+	if screenshot:
+		screenshot_node.texture = screenshot
 
 func _on_pressed() -> void:
 	$"../../../AudioStreamPlayer2D".play()
