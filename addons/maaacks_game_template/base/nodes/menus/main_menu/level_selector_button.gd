801079c3d78ae25572cc3e7dbcd09b1ff70d8805
Andre Correia <andrecorreialagos@gmail.com>
2026-03-22 13:18:48 +0000

    lvl select audio

--- addons/maaacks_game_template/base/nodes/menus/main_menu/level_selector_button.gd
+++ addons/maaacks_game_template/base/nodes/menus/main_menu/level_selector_button.gd
@@ -6,5 +6,6 @@ extends Button
 
 
 func _on_pressed() -> void:
-	$"../../../AudioStreamPlayer2D".play()
+	$AudioStreamPlayer2D.play()
+	await $AudioStreamPlayer2D.finished
 	get_tree().change_scene_to_packed(level_packed)
